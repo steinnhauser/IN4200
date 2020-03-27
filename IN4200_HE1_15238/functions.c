d503b920a4890b678d7811642c1b13e066ed2222
steinnhauser <hausersteinn@gmail.com>
2020-03-27 11:37:36 +0000

    Very solid argument parsing interface implemented

--- IN4200_HE1_15238/functions.c
+++ IN4200_HE1_15238/functions.c
@@ -8,20 +8,30 @@ void arg_parser(int argc, char *argv[], int *tbool, int *pbool, int *dbool, char
     
     The function also takes in a filename argument. An error is raised if there is 
     more than one, and the filename argument is ignored if testing is initiated. */
-    int option; 
+    int option, optind, flags = 0;
+
+    if (argc == 1)
+    {
+        printf("Bad usage: %s requires at least one command line argument.\nTry:\t%s [filename]\nor:\t%s [-d] for default data file,\n\t%s [-t] for testing,\n", argv[0], argv[0], argv[0], argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     while((option = getopt(argc, argv, "tpd")) != -1)
 	{
 		switch(option)
 		{
 			case 't':
 				(*tbool) = 1;
+                flags++;
                 (*filename) = "data/testingdata.txt";
 				break;
 			case 'p':
 				(*pbool) = 1;
+                flags++;
                 break;
             case 'd':
 				(*dbool) = 1;
+                flags++;
                 (*filename) = "data/web-NotreDame.txt";
                 break;
 			default:
@@ -31,19 +41,17 @@ void arg_parser(int argc, char *argv[], int *tbool, int *pbool, int *dbool, char
 	}
 
     /* Count up the extra arguments. The arguments which are not flags are treated
-    as the filename for the data. If there's more than one, raise a syntax error. */
-    int commands = 0;
+    as the filename for the data. Do not overwrite if the [-t] and [-d] flags are parsed. */
     for(; optind < argc; optind++)
     {
         if (!(*tbool) && !(*dbool)) {(*filename) = argv[optind];}
-        commands++;
     } 
 
     /* If more than one are passed raise an error. Exception is for
      testing. Then we use the default filename 'data/testingdata.txt' */
-    if ((commands != 1) && (!(*tbool) & !(*dbool)))
+    if (((argc-flags) != 2) && (!(*tbool) && !(*dbool)))
     {
-        printf("Syntax error: %d arguments passed, expected one for filename.\n", commands);
+        printf("Syntax error: Expected one filename argument.\n");
         exit(EXIT_FAILURE);
     }
 }
